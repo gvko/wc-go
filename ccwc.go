c7b1d952b5bec47a6f4a3db9071e0d9ba727b576
Galin Kostov <dev@galin.cc>
2023-08-04 18:36:27 -0500

    Exit with error if filename not provided

--- ccwc.go
+++ ccwc.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 	cFlag := flag.Bool("c", false, "the command -c")
 	flag.Parse()
+
 	fileName := flag.Arg(0)
+	fileName = strings.TrimSpace(fileName)
+	if fileName == "" {
+		fmt.Println("Error: file name must be provided")
+		os.Exit(1)
+	}
 
 	if *cFlag {
 		println("-c was provided")

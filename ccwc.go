e0ea4b839c5ed08c5b01c303f3c87bef90e6479e
Galin Kostov <dev@galin.cc>
2023-08-05 09:21:40 -0500

    Get filesize. Also refactor checks and other logic into separate functions

--- ccwc.go
+++ ccwc.go
@@ -7,22 +7,38 @@ import (
 	"strings"
 )
 
-func main() {
-	cFlag := flag.Bool("c", false, "the command -c")
-	flag.Parse()
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+}
 
-	fileName := flag.Arg(0)
-	fileName = strings.TrimSpace(fileName)
-	if fileName == "" {
+func checkFilePathGiven(filePath string) {
+	filePath = strings.TrimSpace(filePath)
+	if filePath == "" {
 		fmt.Println("Error: file name must be provided")
 		os.Exit(1)
 	}
+}
+
+func getFileSize(filePath string) int64 {
+	fileInfo, err := os.Stat(filePath)
+	checkErr(err)
+	return fileInfo.Size()
+}
+
+func main() {
+	cFlag := flag.Bool("c", false, "the command -c")
+	flag.Parse()
+	filePath := flag.Arg(0)
+	checkFilePathGiven(filePath)
+	
+	_, err := os.ReadFile(filePath)
+	checkErr(err)
 
 	if *cFlag {
-		println("-c was provided")
-	} else {
-		println("-c was NOT provided")
+		fileSize := getFileSize(filePath)
+		println(fileSize, filePath)
 	}
-
-	println(fileName)
 }
